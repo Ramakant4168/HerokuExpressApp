3006a02dba63f00fd503f220b50fe9e98c79004a
Ramakant4168 <ram4168@gmail.com>
2018-11-18 22:38:30 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = new express();
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('hello welcome to express!!!');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
